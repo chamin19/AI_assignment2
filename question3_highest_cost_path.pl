194bc2c69931b31646cc8a5f021ae04c7dd18c61
camillia <hanaan.amin@yahoo.com>
2022-10-07 12:37:35 -0400

    rough work

--- question3_highest_cost_path.pl
+++ question3_highest_cost_path.pl
@@ -14,19 +14,30 @@
 %%%%% Helper Programs
 % Put any helper programs in the space below
 
+% append two lists together
 append([ ],List,List).
 append([X | List1],List2,[X | Result]) :- append(List1,List2,Result). 
+
+% find maximum in list
+maxim([Max | []], Max).
+maxim([Head, Body | Tail], Max) :- Head >= Body, maxim([Head | Tail], Max).
+maxim([Head, Body | Tail], Max) :- Head < Body, maxim([Body | Tail], Max).
+
+% find maxnode
+maxNode(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), MaxCost, MaxNode) :- LCost >= MCost, LCost >= RCost, MaxCost = LCost, nodeHead(Left, Head), MaxBranch = Left, MaxNode = Head.
+maxNode(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), MaxCost, MaxNode) :- RCost >  MCost, RCost >  LCost, MaxCost = RCost, nodeHead(Right, Head), MaxBranch = Right, MaxNode = Head.
+maxNode(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), MaxCost, MaxNode) :- MCost >  RCost, MCost >  LCost, MaxCost = MCost, nodeHead(Middle, Head), MaxBranch = Middle, MaxNode = Head.
+
+nodeHead(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), Head) :- Head is Name.
 
 % highCostPath(Tree, PathCost, PathList) 
 
 % base case - node has no children
-highestCostPath(tree3(Name, LeftCost, none, MiddleCost, none, RightCost, none), PathCost, PathList) :- append([Name], PathList, Result), PathList is Result, PathCost = PathCost + 0.
+highestCostPath(tree3(Name, LCost, none, MCost, none, RCost, none), Costs, Paths, PathCost, PathList).
 
 % recursive case 
-highestCostPath(tree3(Name, LeftCost, Left, MiddleCost, Middle, RightCost, Right), PathCost, PathList) :- 
-append([Name], PathList, Result), PathCostLeft = Left + PathCost, PathCostMiddle = Right + PathCostMiddle, PathCostRight = PathCost + Right, highestCostPath(tree3(Left),PathCostLeft, PathListLeft) 
-
-
+highestCostPath(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), PathCost, PathList) :- max(tree3(Name, LCost, Left, MCost, Middle, RCost, Right), MaxCost, MaxBranch, MaxNode), 
+append([MaxNode], PathList, Result), Cost is MaxCost + PathCost, highestCostPath(MaxBranch,Cost,Result)).
 
 %%%%% RULE: highestCostPath
 % Add the rule(s) for highestCostPath below
